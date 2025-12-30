bf13c2b203f4d7f8ad07e43bec6ff1852b7d8efb
Roberto Arcomano <bertolinux@gmai.com>
2025-12-30 18:49:34 +0100

    K8ssandra operator working

---
+++ modules/k8ssandra/k8ssandra.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# cert-manager
+# helm repo add jetstack https://charts.jetstack.io
+# helm repo update
+helm install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --set installCRDs=true
+
+# k8ssandra operator
+# helm repo add k8ssandra https://helm.k8ssandra.io/stable
+# helm repo update
+helm install k8ssandra-operator k8ssandra/k8ssandra-operator -n k8ssandra-operator --set global.clusterScoped=true --create-namespace
+# helm install k8ssandra-operator k8ssandra/k8ssandra-operator -n k8ssandra-operator --create-namespace
+sleep 60
+kubectl create -f k8c1.yaml
